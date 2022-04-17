a13a26ef008db779dd417f80435cce0306211996
Aleksandr Shabashov <predresk@rambler.ru>
2022-04-17 13:16:41 +0300

    fix: поменял картинки | почистил код js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: 'Тянем лапку вверх',
-    link: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    name: 'Исаакиевский собор',
+    link: 'https://images.unsplash.com/photo-1555460285-763ba96917d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   },
   {
-    name: 'Сосредоточен',
-    link: 'https://images.unsplash.com/photo-1577023311546-cdc07a8454d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    name: 'Архитектура',
+    link: 'https://images.unsplash.com/photo-1592029328294-dd71c43655c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80'
   },
   {
-    name: 'Бабочка',
-    link: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    name: 'Казанский собор',
+    link: 'https://images.unsplash.com/photo-1551005756-fd0657e8fbf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   },
   {
-    name: 'Хочу спать',
-    link: 'https://images.unsplash.com/photo-1574231164645-d6f0e8553590?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=804&q=80'
+    name: 'Елисеевский проспект',
+    link: 'https://images.unsplash.com/photo-1550643749-d9add3db05e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
   },
   {
-    name: 'Тихий час',
-    link: 'https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    name: 'Статуя князю',
+    link: 'https://images.unsplash.com/photo-1555460285-041a4924bba6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   },
   {
-    name: 'В моменте',
-    link: 'https://images.unsplash.com/photo-1595752776689-aebef37b5d32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    name: 'Колонны',
+    link: 'https://images.unsplash.com/photo-1551270029-4bc0f7eabf78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
   }
 ];
 
@@ -56,8 +56,8 @@ function getElement(item) {
   elementsImage.src = item.link;
   elementsImage.alt = item.name;
 
-  let elementDelete = newCard.querySelector('.element__delete'); 
-  elementDelete.addEventListener('click', deleteElementCard); 
+  let btnDeleteCard = newCard.querySelector('.element__delete'); 
+  btnDeleteCard.addEventListener('click', deleteElementCard); 
 
   function deleteElementCard(evt) {
     let element = evt.target.closest('.elements__card'); 
@@ -65,13 +65,13 @@ function getElement(item) {
   }
 
   let likeButton = newCard.querySelector('.elements__like'); 
-  function handleLikeElements(evt) {
+  function clickLikeElements(evt) {
     let element = evt.target.closest('.elements__card'); 
     let likeButtonActive = element.querySelector('.elements__like'); 
     likeButtonActive.classList.toggle('elements__like_active'); 
   }
 
-  likeButton.addEventListener('click', handleLikeElements); 
+  likeButton.addEventListener('click', clickLikeElements); 
 
   function openPopupFullImage(evt) {
     openPopup(popupPlaceImage);
@@ -79,11 +79,11 @@ function getElement(item) {
     let elementsImage = element.querySelector('.elements__image');
     let popupImage = popupPlaceImage.querySelector('.popup__image');
     let elementsTitle = element.querySelector('.elements__title');
-    let popupHead = popupPlaceImage.querySelector('.popup__head');
+    let popupHeading = popupPlaceImage.querySelector('.popup__heading');
   
     popupImage.src = elementsImage.src;
     popupImage.alt = elementsImage.alt;
-    popupHead.textContent = elementsTitle.textContent;
+    popupHeading.textContent = elementsTitle.textContent;
   }
 
   let btnExit = popupPlaceImage.querySelector('.popup__exit');
@@ -99,8 +99,8 @@ function addNewCard(evt) {
   evt.preventDefault();
   let inputTitle = document.querySelector('.popup__input_type_title').value;
   let inputLink = document.querySelector('.popup__input_type_link').value;
-  let elementQ = getElement({name: inputTitle, link: inputLink});
-  elementsCards.prepend(elementQ);
+  let elementGet = getElement({name: inputTitle, link: inputLink});
+  elementsCards.prepend(elementGet);
 }
 
 
