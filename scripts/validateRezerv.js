ab37e3cfd16a3593b5964ee044abaf664dd13ba2
Aleksandr Shabashov <predresk@rambler.ru>
2022-05-04 18:55:03 +0300

    сделал валидацию формы редактирования

--- scripts/validateRezerv.js
+++ scripts/validateRezerv.js
@@ -1,17 +1,7 @@
 
-const showErrorIn = (input, config) => {
-  const errorNode = document.querySelector(`#${input.id}-error`);
-  errorNode.textContent = input.validationMessage;
-  errorNode.classList.add(config.errorClass);
-  input.classList.add(config.inputErrorClass);
-};
+//тут всё из тренажера, 
+//работает только один попа редактирования прфиля
 
-const hideErrorIn = (input, config) => {
-  const errorNode = document.querySelector(`#${input.id}-error`);
-  errorNode.textContent ='';
-  errorNode.classList.remove(config.errorClass);
-  input.classList.remove(config.inputErrorClass);
-};
 
 function enableValidation(config) {
   const form = document.querySelector(config.formSelector);
@@ -29,7 +19,7 @@ function enableValidation(config) {
 function toggleButton(form, config) {
   const button = document.querySelector(config.submitButtonSelector);
   button.disabled = !form.checkValidity();
- button.classList.toggle('popup__button_disabled', !form.checkValidity());
+  button.classList.toggle('popup__button_disabled', !form.checkValidity());
 }
 
 function handleFormSubmit(event, form) {
@@ -51,8 +41,8 @@ function handleFormInput(event, form, config) {
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button', // кнопка 
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error', //красная линия у инпута
-  errorClass: 'popup__error_visible'
+  submitButtonSelector: '.popup__button',
+  // inactiveButtonClass: 'popup__button_disabled',
+  // inputErrorClass: 'popup__input_type_error',
+  // errorClass: 'popup__error_visible'
 }); 
